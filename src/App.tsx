e9fd676f9648265a4d3c4f867bfcf8ae58f08a3a
Abdullah <abdulhhany69@gmail.com>
2025-05-19 01:51:23 +0300

    Enhance add product modal form

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import ProductCard from "./components/ProductCard";
 import Modal from "./components/ui/Modal";
-import { productList } from "./data";
+import { formInputsList, productList } from "./data";
 import Button from "./components/ui/Button";
+import Input from "./components/ui/Input";
 
 function App() {
   // ** STATE
@@ -20,18 +21,36 @@ function App() {
   const renderProductsList = productList.map((product) => (
     <ProductCard key={product.id} product={product} />
   ));
+  const renderFormInputList = formInputsList.map((input) => (
+    <div className="flex flex-col">
+      <label
+        htmlFor={input.id}
+        className="text-gray-700 font-medium text-sm mb-[2px]"
+      >
+        {input.label}
+      </label>
+      <Input type="text" id={input.id} name={input.name} />{" "}
+    </div>
+  ));
 
   return (
     <main className="container">
-          <Button className="bg-indigo-700 hover:bg-indigo-900" onClick={openModal}>ADD</Button>
+      <Button className="bg-indigo-700 hover:bg-indigo-900" onClick={openModal}>
+        ADD
+      </Button>
       <div className="m-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 md:gap-4 p-2 rounded-md">
         {renderProductsList}
       </div>
       <Modal isOpen={isOpen} closeModal={closeModal} title="ADD A NEW PRODUCT">
-        <div className="flex items-center space-x-3" >
-          <Button className="bg-indigo-700 hover:bg-indigo-900">Submit</Button>
-          <Button className="bg-gray-400 hover:bg-gray-600">Cancel</Button>
-        </div>
+        <form className="space-y-3">
+          {renderFormInputList}{" "}
+          <div className="flex items-center space-x-3">
+            <Button className="bg-indigo-700 hover:bg-indigo-900">
+              Submit
+            </Button>
+            <Button className="bg-gray-400 hover:bg-gray-500">Cancel</Button>
+          </div>
+        </form>
       </Modal>
     </main>
   );
--- src/data/index.ts
+++ src/data/index.ts
@@ -11,6 +11,13 @@ export interface IProduct {
   };
 }
 
+export interface IFormInput {
+  id: string;
+  name: string;
+  label: string;
+  type: string;
+}
+
 export const productList: IProduct[] = [
   {
     id: 1,
@@ -153,5 +160,32 @@ export const productList: IProduct[] = [
       imageURL:
         "https://images.unsplash.com/photo-1583121274602-3e2820c69888?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
     },
+  },
+];
+
+export const formInputsList: IFormInput[] = [
+  {
+    id: "title",
+    name: "title",
+    label: "Product Title",
+    type: "text",
+  },
+  {
+    id: "description",
+    name: "description",
+    label: "Product Description",
+    type: "text",
+  },
+  {
+    id: "image",
+    name: "imageURL",
+    label: "Product Image URL",
+    type: "text",
+  },
+  {
+    id: "price",
+    name: "price",
+    label: "Product Price",
+    type: "text",
   },
 ];
